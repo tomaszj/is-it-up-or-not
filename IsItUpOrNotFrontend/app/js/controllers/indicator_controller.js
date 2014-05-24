cdc802049cc6dabd0339b2bdfd6eb50428f21316
Tomasz Janeczko <janecztom@gmail.com>
2014-05-24 12:06:28 +0100

    Added plugin, lint fix in IndicatorController.

--- IsItUpOrNotFrontend/app/js/controllers/indicator_controller.js
+++ IsItUpOrNotFrontend/app/js/controllers/indicator_controller.js
@@ -5,20 +5,20 @@ angular.module("app").controller("IndicatorController", function ($scope, $locat
 
   $scope.isInEditMode = function() {
     return isInEditMode;
-  }
+  };
 
   $scope.turnOnEditMode = function() {
     isInEditMode = true;
-  }
+  };
 
   $scope.cancelEditMode = function() {
     isInEditMode = false; 
-  }
+  };
 
   $scope.commitEditModeWithFlag = function(flag) {
     flag.$update(); 
     isInEditMode = false; 
-  }
+  };
 
   $scope.isFlagUp = function(flag) {
     return flag.state === 'up';
