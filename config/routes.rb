bae74ec77c41d609fe97a45edb960df70b1dc26f
Tomasz Janeczko <janecztom@gmail.com>
2014-05-24 15:10:55 +0100

    Clean-up done to allow faster Heroku deployment after frontend changes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
     end
   end
   
-  match '(*all_params)', to: 'static#main', via: :get
+  # match '(*all_params)', to: 'static#main', via: :get
   
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#main'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
