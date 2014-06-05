49d961072ebb642b630923705b39e9eddd1535fa
Tomasz Janeczko <t.janeczko@kainos.com>
2014-06-05 11:13:42 +0100

    Revert "Refactored resolvers."
    This reverts commit 5657c644cfc4b7d643043560325da4ed6d552c83.

--- frontend_app/app/js/controllers/flag_controller.js
+++ frontend_app/app/js/controllers/flag_controller.js
@@ -1,17 +1,4 @@
-var FlagController = angular.module("app").controller("FlagController", function (flag, $scope, $routeParams, FlagResource) {
+angular.module("app").controller("FlagController", function (flag, $scope, $routeParams, FlagResource) {
   $scope.flag = flag;
 });
-
-FlagController.resolve = {
-  flag: ['FlagService', '$q', '$route', function(FlagService, $q, $route) {
-    var defer = $q.defer();
-    FlagService.find($route.current.params.id).$promise.then(function(flag) {
-      return defer.resolve(flag); 
-    }, function (err) {
-      console.log(err);
-      return defer.reject({});
-    });
-    return defer.promise;
-  }]
-};
 
--- frontend_app/app/js/controllers/flags_controller.js
+++ frontend_app/app/js/controllers/flags_controller.js
@@ -1,17 +1,4 @@
-var FlagsController = angular.module("app").controller("FlagsController", function (flags, $scope, $routeParams, FlagResource) {
+angular.module("app").controller("FlagsController", function (flags, $scope, $routeParams, FlagResource) {
   $scope.flags = flags;
 });
-
-FlagsController.resolve = {
-  flags: ['FlagService', '$q', function(FlagService, $q) {
-    var defer = $q.defer();
-    FlagService.findAll().$promise.then(function (flags) {
-      return defer.resolve(flags);
-    }, function(err) {
-      console.log(err);
-      return defer.reject({});
-    });
-    return defer.promise;
-  }]
-};
 
