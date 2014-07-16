93dbd50787095df237bcf6860dd1efc187d47adc
Tomasz Janeczko <t.janeczko@kainos.com>
2014-07-16 14:35:30 +0100

    Added basic authentication that is provided through env variables
    - Use BASIC_HTTP_AUTH_USERNAME and BASIC_HTTP_AUTH_PASSWORD to set that
    - Details can be found in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+
+  BASIC_HTTP_AUTH_USERNAME = "BASIC_HTTP_AUTH_USERNAME"
+  BASIC_HTTP_AUTH_PASSWORD = "BASIC_HTTP_AUTH_PASSWORD"
+  
+  unless ENV[BASIC_HTTP_AUTH_USERNAME].blank? || ENV[BASIC_HTTP_AUTH_PASSWORD].blank?
+    http_basic_authenticate_with name: ENV[BASIC_HTTP_AUTH_USERNAME], password: ENV[BASIC_HTTP_AUTH_PASSWORD]
+  end
 end
